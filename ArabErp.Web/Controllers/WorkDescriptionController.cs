bf6fd6be788bdf9ecc417c1efa44dfcd2125ccb3
sebin <sebin@SEBINSPC>
2016-05-06 18:05:55 +0530

    wrk description controller

---
+++ ArabErp.Web/Controllers/WorkDescriptionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class WorkDescriptionController : Controller
+    {
+        // GET: WorkDescription
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateWorkDescription()
+        {
+            return View();
+        }
+    }
+}
