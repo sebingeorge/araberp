f3d8dd2ca16046bc3ab7114623544406a8be1496
sebin <sebin@SEBINSPC>
2016-05-10 09:26:10 +0530

    Currency Master

---
+++ ArabErp.Web/Controllers/CurrencyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        // GET: Currency
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
