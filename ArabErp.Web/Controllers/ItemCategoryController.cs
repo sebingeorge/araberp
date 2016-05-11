768c4d5ab2042892ec560d040234bee770c7488d
sebin <sebin@SEBINSPC>
2016-05-11 09:21:20 +0530

    Unit Master

---
+++ ArabErp.Web/Controllers/ItemCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class ItemCategoryController : Controller
+    {
+        // GET: ItemCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
