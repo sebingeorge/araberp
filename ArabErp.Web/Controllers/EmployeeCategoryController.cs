d3880bd1c2401f8240566ca4f176dbe5237fce47
sebin <sebin@SEBINSPC>
2016-05-12 09:47:28 +0530

    Masters

---
+++ ArabErp.Web/Controllers/EmployeeCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class EmployeeCategoryController : Controller
+    {
+        // GET: EmployeeCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult create()
+        {
+            return View();
+        }
+    }
+}
