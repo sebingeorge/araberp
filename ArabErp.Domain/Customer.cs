8aca10e43b044ea8a7350bf9c71fffff2edfcd49
sebin <sebin@SEBINSPC>
2016-05-12 10:37:50 +0530

    Customer and Item Master design and model binding

--- ArabErp.Domain/Customer.cs
+++ ArabErp.Domain/Customer.cs
@@ -10,12 +10,34 @@ namespace ArabErp.Domain
     {
         public int CustomerId { get; set; }
         public string CustomerName { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
+
+        public string CustomerPrintName { get; set; }
+        public int CategoryId { get; set; }
+
+        public int LeadSourceId { get; set; }
+        public int CurrencyId { get; set; }
+        public string DoorNo { get; set; }
+        public string Street { get; set; }
+        public string State { get; set; }
+        public int Country { get; set; }
+        public string Zip { get; set; }
         public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string ContactPerson { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int OrganizationId { get; set; }
     }
+   public enum CategoryId
+   { Category1, Category2}
+
+   public enum LeadSourceId
+   { Lead_Source1, Lead_Source2 }
+
+   public enum CurrencyId
+   { Currency1, Currency2 }
+
+   public enum Country
+   { Country1, Country2 }
 }
