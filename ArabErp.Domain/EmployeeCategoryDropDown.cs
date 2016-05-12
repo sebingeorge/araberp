3b38745f9de7fecae1b85f64223a08071f77f8b4
sebin <sebin@SEBINSPC>
2016-05-12 11:54:47 +0530

    EmployeeCategoryDropdown-Midhu

---
+++ ArabErp.Domain/EmployeeCategoryDropDown.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+ public class EmployeeCategoryDropDown
+    {
+     //public IEnumerable<SelectListItem> Data { get; set; }
+     //public int EmpCategoryId { get; set; }
+
+        public int? Code { get; set; }
+        public string Name { get; set; }
+     
+    }
+}
---
+++ ArabErp.Web/Controllers/DropDownController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArabErp.Domain;
+using ArabErp.DAL;
+
+namespace ArabErp.Web.Controllers
+{
+    public class DropDownController : Controller
+    {
+        EmployeeRepository _EmpRepo;
+        public DropDownController(EmployeeRepository EmpRepo)
+        {
+            _EmpRepo = EmpRepo;
+        }
+        // GET: DropDown
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EmployeeCategory()
+        {
+            var list = _EmpRepo.CategoryDropdownList();
+            List<EmployeeCategoryDropDown> Category = new List<EmployeeCategoryDropDown>();
+            Category.Add(new EmployeeCategoryDropDown
+            {
+                Code = 0,
+                Name = "All"
+            });
+            foreach (var item in list)
+            {
+                Category.Add(item);
+            }
+            return PartialView("_EmployeeCategory", Category);
+        }
+
+      
+    }
+}
