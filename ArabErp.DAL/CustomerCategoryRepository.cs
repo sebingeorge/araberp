0eaf36f021b382fd4f889e89867a45669c6168d7
praseetha <praseetha@user>
2016-05-17 17:51:18 +0530

    repository

---
+++ ArabErp.DAL/CustomerCategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using ArabErp.Domain;
+
+namespace ArabErp.DAL
+{
+    public class CustomerCategoryRepository
+    {
+         private SqlConnection connection;
+
+         public CustomerCategoryRepository()
+        {
+            if (connection == null)
+            {
+                connection = ConnectionManager.connection;
+            }
+        }
+
+         public int InsertCustomerCategory(CustomerCategory objCustomerCategory)
+         {
+             string sql = @"INSERT INTO CustomerCategory(CusCategoryRefNo,CusCategoryName,CusCategoryShortName,CreatedBy,CreatedDate,OrganizationId) VALUES(@CusCategoryRefNo,@CusCategoryName,@CusCategoryShortName,@CreatedBy,getDate(),@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+             var id = connection.Query<int>(sql, objCustomerCategory).Single();
+             return id;
+         }
+
+         public CustomerCategory GetCustomerCategory(int CustomerCategoryId)
+         {
+
+             string sql = @"select * from CustomerCategory
+                                    where CusCategoryId=@CusCategoryId";
+
+             var objCustomerCategory = connection.Query<CustomerCategory>(sql, new
+             {
+                 CustomerCategoryId = CustomerCategoryId
+             }).First<CustomerCategory>();
+
+             return objCustomerCategory;
+         }
+
+         public List<CustomerCategory> GetCustomerCategory()
+         {
+             string sql = @"select * from CustomerCategory
+                        where OrganizationId>0";
+
+             var objCustomerCategory = connection.Query<CustomerCategory>(sql).ToList<CustomerCategory>();
+
+             return objCustomerCategory;
+         }
+
+         public void Dispose()
+         {
+             connection.Dispose();
+         }
+    }
+}
---
+++ ArabErp.DAL/DesignationRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+
+namespace ArabErp.DAL
+{
+    public class DesignationRepository : BaseRepository
+    {
+        public int InsertDesignation(Designation objDesignation)
+        {
+            string sql = @"INSERT INTO Designation(DesignationRefNo,DesignationName,CreatedBy,CreatedDate,OrganizationId) VALUES(@DesignationRefNo,@DesignationName,@CreatedBy,@CreatedDate,@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+            var id = connection.Query<int>(sql, objDesignation).Single();
+            return id;
+        }
+
+        public Designation GetDesignation(int DesignationId)
+        {
+
+            string sql = @"select * from Designation
+                        where DesignationId=@DesignationId";
+
+            var objDesignation = connection.Query<Designation>(sql, new
+            {
+                DesignationId = DesignationId
+            }).First<Designation>();
+
+            return objDesignation;
+        }
+
+        public List<Designation> GetDesignation()
+        {
+            string sql = @"select * from Designation
+                        where OrganizationId>0";
+
+            var objDesignation = connection.Query<Designation>(sql).ToList<Designation>();
+
+            return objDesignation;
+        }
+
+    }
+  
+}
