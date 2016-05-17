cfe05bbae55d0e1ed6f135f95775dad7ef809425
midhu <midhugeorge>
2016-05-17 14:14:56 +0530

    Currency modification

--- ArabErp.Domain/Currency.cs
+++ ArabErp.Domain/Currency.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace ArabErp.Domain
 {
     public class Currency
     {
+        
         public int CurrencyId { get; set; }
+        [Required(ErrorMessage = "Please Enter Code")]
         public string CurrencyRefNo { get; set; }
+         [Required(ErrorMessage = "Please Enter Name")]
         public string CurrencyName { get; set; }
+         [Required(ErrorMessage = "Please Enter Elementary")]
         public string Elementary { get; set; }
+         [Required(ErrorMessage = "Please Enter Exchange Rate")]
         public string CurrencyExRate { get; set; }
         public int CurrencySymbolId { get; set; }
         public string CreatedBy { get; set; }
@@ -25,4 +33,5 @@ namespace ArabErp.Domain
         public int SymbolId { get; set; }
         public string SymbolName { get; set; }
 }
+
 }
