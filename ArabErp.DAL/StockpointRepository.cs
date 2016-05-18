0f70a29d3dca46b5b80c12b624ba31d246959c93
sebin <sebin@SEBINSPC>
2016-05-18 11:47:08 +0530

    Stockpoint Rep

---
+++ ArabErp.DAL/StockpointRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+
+namespace ArabErp.DAL
+{
+    public class StockpointRepository : BaseRepository
+    {
+
+        public int InsertStockpoint(Stockpoint objStockpoint)
+        {
+            string sql = @"insert  into StockPoint(StockPointRefNo,StockPointName,StockPointShrtName,StockPointDoorNo,StockPointZip,StockPointArea,StockPointPhone,StockPointCity,StockPointFax,CreatedBy,CreatedDate,OrganizationId) Values (@StockPointRefNo,@StockPointName,@StockPointShrtName,@StockPointDoorNo,@StockPointZip,@StockPointArea,@StockPointPhone,@StockPointCity,@StockPointFax,@CreatedBy,@CreatedDate,@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+
+            var id = connection.Query<int>(sql, objStockpoint).Single();
+            return id;
+        }
+
+
+        public Stockpoint GetStockpoint(int StockpointId)
+        {
+
+            string sql = @"select * from Stockpoint
+                        where StockpointId=@StockpointId";
+
+            var objStockpoint = connection.Query<Stockpoint>(sql, new
+            {
+                StockpointId = StockpointId
+            }).First<Stockpoint>();
+
+            return objStockpoint;
+        }
+
+        public List<Stockpoint> GetStockpoints()
+        {
+            string sql = @"select * from Stockpoint
+                        where isActive=1";
+
+            var objStockpoints = connection.Query<Stockpoint>(sql).ToList<Stockpoint>();
+
+            return objStockpoints;
+        }
+
+        public int UpdateStockpoint(Stockpoint objStockpoint)
+        {
+            string sql = @"UPDATE StockPoint SET StockPointRefNo = @StockPointRefNo ,StockPointName = @StockPointName ,StockPointShrtName = @StockPointShrtName ,StockPointDoorNo = @StockPointDoorNo,StockPointZip = @StockPointZip,StockPointArea = @StockPointArea,StockPointPhone = @StockPointPhone,StockPointCity = @StockPointCity,StockPointFax = @StockPointFax  OUTPUT INSERTED.StockPointId  WHERE StockPointId = @StockPointId";
+
+
+            var id = connection.Execute(sql, objStockpoint);
+            return id;
+        }
+
+        public int DeleteStockpoint(Unit objStockpoint)
+        {
+            string sql = @"Delete Stockpoint  OUTPUT DELETED.StockpointId WHERE StockpointId=@StockpointId";
+
+
+            var id = connection.Execute(sql, objStockpoint);
+            return id;
+        }
+
+
+    }
+}
