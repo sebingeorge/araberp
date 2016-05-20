53dcb53f70a3f1047799193e014e5c99f0824076
sebin <sebin@SEBINSPC>
2016-05-20 14:03:06 +0530

    WarehouseRep.Item Rep

--- ArabErp.DAL/ItemRepository.cs
+++ ArabErp.DAL/ItemRepository.cs
@@ -16,5 +16,58 @@ namespace ArabErp.DAL
            return list;
             
         }
+        public int InsertItem(Item objItem)
+        {
+            string sql = @"insert  into Item(PartNo,ItemName,ItemPrintName,ItemShortName,ItemGroupId,ItemSubGroupId,ItemCategoryId,ItemUnitId,ItemQualityId,CommodityId,MinLevel,ReorderLevel,MaxLevel,OrganizationId) Values (@PartNo,@ItemName,@ItemPrintName,@ItemShortName,@ItemGroupId,@ItemSubGroupId,@ItemCategoryId,@ItemUnitId,@ItemQualityId,@CommodityId,@MinLevel,@ReorderLevel,@MaxLevel,@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+
+            var id = connection.Query<int>(sql, objItem).Single();
+            return id;
+        }
+
+
+        public Item GetItem(int ItemId)
+        {
+
+            string sql = @"select * from Item
+                        where ItemId=@ItemId";
+
+            var objItem = connection.Query<Item>(sql, new
+            {
+                ItemId = ItemId
+            }).First<Item>();
+
+            return objItem;
+        }
+
+        public List<Item> GetItems()
+        {
+            string sql = @"select * from Item
+                        where isActive=1";
+
+            var objItems = connection.Query<Item>(sql).ToList<Item>();
+
+            return objItems;
+        }
+
+        public int UpdateItem(Item objItem)
+        {
+            string sql = @"UPDATE Item SET PartNo = @PartNo ,ItemName = @ItemName ,ItemPrintName = @ItemPrintName ,ItemShortName = @ItemShortName,ItemGroupId = @ItemGroupId,ItemSubGroupId = @ItemSubGroupId,ItemCategoryId = @ItemCategoryId,ItemUnitId = @ItemUnitId,ItemQualityId = @ItemQualityId  OUTPUT INSERTED.ItemId  WHERE ItemId = @ItemId";
+
+
+            var id = connection.Execute(sql, objItem);
+            return id;
+        }
+
+        public int DeleteItem(Unit objItem)
+        {
+            string sql = @"Delete Item  OUTPUT DELETED.ItemId WHERE ItemId=@ItemId";
+
+
+            var id = connection.Execute(sql, objItem);
+            return id;
+        }
+
     }
 }
---
+++ ArabErp.DAL/WareHouseRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+
+namespace ArabErp.DAL
+{
+    public class WareHouseRepository : BaseRepository
+    {
+
+        public int InsertWareHouse(WareHouse objWareHouse)
+        {
+            string sql = @"insert  into WareHouse(WareHouseNo,WareHouseDescription,CreatedBy,CreatedDate,OrganizationId) Values (@WareHouseNo,@WareHouseDescription,@CreatedBy,@CreatedDate,@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+
+            var id = connection.Query<int>(sql, objWareHouse).Single();
+            return id;
+        }
+
+
+        public WareHouse GetWareHouse(int WareHouseId)
+        {
+
+            string sql = @"select * from WareHouse
+                        where WareHouseId=@WareHouseId";
+
+            var objWareHouse = connection.Query<WareHouse>(sql, new
+            {
+                WareHouseId = WareHouseId
+            }).First<WareHouse>();
+
+            return objWareHouse;
+        }
+
+        public List<WareHouse> GetWareHouses()
+        {
+            string sql = @"select * from WareHouse
+                        where isActive=1";
+
+            var objWareHouses = connection.Query<WareHouse>(sql).ToList<WareHouse>();
+
+            return objWareHouses;
+        }
+
+        public int UpdateWareHouse(WareHouse objWareHouse)
+        {
+            string sql = @"UPDATE WareHouse SET WareHouseNo = @WareHouseNo ,WareHouseDescription = @WareHouseDescription ,CreatedBy = @CreatedBy ,CreatedDate = @CreatedDate  OUTPUT INSERTED.WareHouseId  WHERE WareHouseId = @WareHouseId";
+
+
+            var id = connection.Execute(sql, objWareHouse);
+            return id;
+        }
+
+        public int DeleteWareHouse(Unit objWareHouse)
+        {
+            string sql = @"Delete WareHouse  OUTPUT DELETED.WareHouseId WHERE WareHouseId=@WareHouseId";
+
+
+            var id = connection.Execute(sql, objWareHouse);
+            return id;
+        }
+
+
+    }
+}
