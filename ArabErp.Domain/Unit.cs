2e1bc3ae925dfd769c7375c21b38d77f3961ec7d
praseetha <praseetha@user>
2016-05-20 10:57:20 +0530

    Unit,itemCategory,ItemGroup Validations

--- ArabErp.Domain/Unit.cs
+++ ArabErp.Domain/Unit.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace ArabErp.Domain
 {
    public class Unit
     {
        public int UnitId { get; set; }
+       [Required(ErrorMessage = "Please Enter Code")]
        public string UnitRefNo { get; set; }
+       [Required(ErrorMessage = "Please Enter Ref.No")]
        public string UnitName { get; set; }
+       [Required(ErrorMessage = "Please Enter Name")]
        public string CreatedBy { get; set; }
        public DateTime? CreatedDate { get; set; }
        public int OrganizationId { get; set; }
