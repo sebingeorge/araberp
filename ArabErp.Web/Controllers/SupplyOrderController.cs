f5d2f6440cab015fd24040360930c8eb8bb91937
midhu <midhugeorge>
2016-05-26 15:31:24 +0530

    transaction form design change

--- ArabErp.Web/Controllers/SupplyOrderController.cs
+++ ArabErp.Web/Controllers/SupplyOrderController.cs
@@ -13,7 +13,7 @@ namespace ArabErp.Web.Controllers
         {
             return View();
         }
-        public ActionResult CreateSupplyOrder()
+        public ActionResult Create()
         {
             return View();
         }
