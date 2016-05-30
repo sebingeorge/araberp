593aeccb238ef171bf1681360f6c20a3e2dbe24d
sebin <sebin@SEBINSPC>
2016-05-30 17:43:10 +0530

    Workdescrption dal change

--- ArabErp.Domain/WorkDescription.cs
+++ ArabErp.Domain/WorkDescription.cs
@@ -9,6 +9,9 @@ namespace ArabErp.Domain
    public class WorkDescription
     {
         public int WorkDescriptionId { get; set; }
+        public int VehicleModelId { get; set; }
+        public int FreezerUnitId { get; set; }
+        public int BoxId { get; set; }
         public string WorkDescr { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
