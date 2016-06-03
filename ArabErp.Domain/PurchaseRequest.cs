c09701cf10dea92050b58312f9db922681a8dc48
midhu <midhugeorge>
2016-06-03 15:31:51 +0530

    Purchase Request Incomplete

--- ArabErp.Domain/PurchaseRequest.cs
+++ ArabErp.Domain/PurchaseRequest.cs
@@ -12,10 +12,16 @@ namespace ArabErp.Domain
         public string PurchaseRequestNo { get; set; }
         public DateTime? PurchaseRequestDate { get; set; }
         public int WorkShopRequestId { get; set; }
+        public string WorkShopRequestNo { get; set; }
+        public string SaleOrderRefNo { get; set; }
+        public string CustomerOrderRef { get; set; }
+        public string CustomerName { get; set; }
         public string SpecialRemarks { get; set; }
         public DateTime? RequiredDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int OrganizationId { get; set; }
+        public List<PurchaseRequestItem>items { get; set; }
+
     }
 }
