5e8ddb13b049f1f14a04d2a4705f98f5fb0dd33e
sebin <sebin@SEBINSPC>
2016-06-03 11:40:23 +0530

    Supplyorder prnding started

--- ArabErp.Web/Controllers/SupplyOrderController.cs
+++ ArabErp.Web/Controllers/SupplyOrderController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArabErp.DAL;
+using ArabErp.Domain;
 
 namespace ArabErp.Web.Controllers
 {
@@ -19,7 +21,12 @@ namespace ArabErp.Web.Controllers
         }
         public ActionResult PendingSupplyOrder()
         {
-            return View();
+
+            SupplyOrderRepository rep = new SupplyOrderRepository();
+
+            IEnumerable<PendingPurchaseRequest> model = rep.GetPendingPurchaseRequest();
+
+            return View(model);
         }
     }
 }
