ca61b46abb17c2e45e64af05649fa9508b41fb5d
sebin <sebin@SEBINSPC>
2016-06-07 12:49:30 +0530

    Workshop GRN

---
+++ ArabErp.Domain/WorkShopGRNItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+  public  class WorkShopGRNItem
+    {
+        public int WorkShopGRNItemId { get; set; }
+        public int GRNId { get; set; }
+        public int? SlNo { get; set; }
+        public int ItemId { get; set; }
+        public string ItemDescription { get; set; }
+        public string PartNo { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Amount { get; set; }
+        public bool isActive { get; set; }
+        public string Remarks { get; set; }
+    }
+}
