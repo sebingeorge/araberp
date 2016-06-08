e41ea0c72b10ae0ba32fbe2891ff48bcbbff455a
Sreekanth <Sreekanth@SreekanthPC>
2016-06-08 11:22:43 +0530

    Bay Status Report Changes

--- ArabErp.Domain/BayStatus.cs
+++ ArabErp.Domain/BayStatus.cs
@@ -11,14 +11,14 @@ namespace ArabErp.Domain
         public int BayId { get; set; }
         public string BayName { get; set; }
         public string Occupied { get; set; }
-        public DateTime JobCardDate { get; set; }
+        public DateTime? JobCardDate { get; set; }
         public string JobCardNo { get; set; }
         public string ChasisNoRegNo { get; set; }
         public string VehicleModelName { get; set; }
         public string UnitName { get; set; }
         public string WorkDescription { get; set; }
-        public DateTime EDateDelivery { get; set; }
+        public DateTime? EDateDelivery { get; set; }
         public string CustomerName { get; set; }
-        public DateTime EDateArrival { get; set; }
+        public DateTime? EDateArrival { get; set; }
     }
 }
