30501a8a58d9ffd22fe083c53fbb632ae6d1ca17
Shanid <praseetha@user>
2016-06-09 17:06:03 +0530

    models for local purchase

---
+++ ArabErp.Domain/LocalPurchaseGRNItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+    public class LocalPurchaseGRNItem
+    {
+        public int LocalPurchaseGRNItemId { get; set; }
+        public int LocalPurchaseGRNId { get; set; }
+        public int? SupplyOrderItemId { get; set; }
+        public int? SlNo { get; set; }
+        public string ItemName { get; set; }
+        public int? ItemId { get; set; }
+        public string ItemDescription { get; set; }
+        public string Remarks { get; set; }
+        public string PartNo { get; set; }
+        public decimal? PendingQuantity { get; set; }
+        public decimal? Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal? Rate { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
---
+++ ArabErp.Domain/LocalPurchaseRequestItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+    public class LocalPurchaseRequestItem
+    {
+        public int PurchaseRequestItemId { get; set; }
+        public int PurchaseRequestId { get; set; }
+        public int? SlNo { get; set; }
+        public string ItemName { get; set; }
+        public int? ItemId { get; set; }
+        public string Remarks { get; set; }
+        public decimal? Quantity { get; set; }
+        public decimal? Rate { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
