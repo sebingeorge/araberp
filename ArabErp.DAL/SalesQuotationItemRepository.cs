679507b12c990bcf8b36d7887e2909e50f6ba33e
sebin <sebin@SEBINSPC>
2016-06-15 14:47:43 +0530

    SalesQuotation Class And Repo

---
+++ ArabErp.DAL/SalesQuotationItemRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+using System.Data;
+
+namespace ArabErp.DAL
+{
+    public class SalesQuotationItemRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("arab");
+        public int InsertSalesQuotationItem(SalesQuotationItem objSalesQuotationItem)
+        {
+
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"insert  into SalesQuotationItem(SalesQuotationId,SlNo,WorkDescriptionId,Remarks,PartNo,Quantity,UnitId,Rate,Discount,Amount,OrganizationId) Values (@SalesQuotationId,@SlNo,@WorkDescriptionId,@Remarks,@PartNo,@Quantity,@UnitId,@Rate,@Discount,@Amount,@OrganizationId);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+
+                var id = connection.Query<int>(sql, objSalesQuotationItem).Single();
+                return id;
+            }
+        }
+
+
+        public SalesQuotationItem GetSalesQuotationItem(int SalesQuotationItemId)
+        {
+
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from SalesQuotationItem
+                        where SalesQuotationItemId=@SalesQuotationItemId";
+
+                var objSalesQuotationItem = connection.Query<SalesQuotationItem>(sql, new
+                {
+                    SalesQuotationItemId = SalesQuotationItemId
+                }).First<SalesQuotationItem>();
+
+                return objSalesQuotationItem;
+            }
+        }
+
+        public List<SalesQuotationItem> GetSalesQuotationItems()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from SalesQuotationItem
+                        where isActive=1";
+
+                var objSalesQuotationItems = connection.Query<SalesQuotationItem>(sql).ToList<SalesQuotationItem>();
+
+                return objSalesQuotationItems;
+            }
+        }
+
+
+
+        public int DeleteSalesQuotationItem(Unit objSalesQuotationItem)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"Delete SalesQuotationItem  OUTPUT DELETED.SalesQuotationItemId WHERE SalesQuotationItemId=@SalesQuotationItemId";
+
+
+                var id = connection.Execute(sql, objSalesQuotationItem);
+                return id;
+            }
+        }
+
+
+    }
+}
---
+++ ArabErp.DAL/SalesQuotationRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+using System.Data;
+
+namespace ArabErp.DAL
+{
+    public class SalesQuotationRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("arab");
+        public int InsertSalesQuotation(SalesQuotation objSalesQuotation)
+        {
+
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"insert  into SalesQuotation(QuotationRefNo,QuotationDate,CustomerId,ContactPerson,SalesExecutiveId,PredictedClosingDate,QuotationValidToDate,ExpectedDeliveryDate,IsQuotationApproved,ApprovedBy,Amount,QuotationStatus,Remarks,SalesQuotationRejectReasonId,QuotationRejectReason,Competitors,PaymentTerms,DiscountRemarks,CreatedBy,CreatedDate) Values (@QuotationRefNo,@QuotationDate,@CustomerId,@ContactPerson,@SalesExecutiveId,@PredictedClosingDate,@QuotationValidToDate,@ExpectedDeliveryDate,@IsQuotationApproved,@ApprovedBy,@Amount,@QuotationStatus,@Remarks,@SalesQuotationRejectReasonId,@QuotationRejectReason,@Competitors,@PaymentTerms,@DiscountRemarks,@CreatedBy,@CreatedDate);
+            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+
+                var id = connection.Query<int>(sql, objSalesQuotation).Single();
+                return id;
+            }
+        }
+
+
+        public SalesQuotation GetSalesQuotation(int SalesQuotationId)
+        {
+
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from SalesQuotation
+                        where SalesQuotationId=@SalesQuotationId";
+
+                var objSalesQuotation = connection.Query<SalesQuotation>(sql, new
+                {
+                    SalesQuotationId = SalesQuotationId
+                }).First<SalesQuotation>();
+
+                return objSalesQuotation;
+            }
+        }
+
+        public List<SalesQuotation> GetSalesQuotations()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from SalesQuotation
+                        where isActive=1";
+
+                var objSalesQuotations = connection.Query<SalesQuotation>(sql).ToList<SalesQuotation>();
+
+                return objSalesQuotations;
+            }
+        }
+
+
+
+        public int DeleteSalesQuotation(Unit objSalesQuotation)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"Delete SalesQuotation  OUTPUT DELETED.SalesQuotationId WHERE SalesQuotationId=@SalesQuotationId";
+
+
+                var id = connection.Execute(sql, objSalesQuotation);
+                return id;
+            }
+        }
+
+
+    }
+}
