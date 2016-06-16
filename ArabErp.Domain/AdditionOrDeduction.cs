f967f072c0e196469ce4b636e6186969828b9607
Sreekanth <Sreekanth@SreekanthPC>
2016-06-16 11:18:23 +0530

    changes

---
+++ ArabErp.Domain/AdditionOrDeduction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+    public class AdditionOrDeduction
+    {
+        public int? AddDedId { get; set; }
+        [Required]
+        public string AddDedName { get; set; }
+        [Required]
+        public int? AddDedType { get; set; }
+        public string AddDedRemarks { get; set; }
+    }
+}
