85cefa6e4ef50169655a897572ad0cffdce93d99
Shanid <praseetha@user>
2016-06-23 12:53:26 +0530

    consumption

---
+++ ArabErp.Domain/ConsumptionItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+    public class ConsumptionItem
+    {
+        public int? ConsumptionItemId { get; set; }
+        public int ConsumptionId { get; set; }
+        public int ItemId { get; set; }
+        public string PartNo { get; set; }
+        public string Remarks { get; set; }
+        public decimal Amount { get; set; }
+        public bool isActive { get; set; }
+    }
+}
