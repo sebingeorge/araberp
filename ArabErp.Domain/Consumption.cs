7da3f43d3f3593b68e09cf9c32e3a317595a1bf7
Shanid <praseetha@user>
2016-06-27 12:58:18 +0530

    consumption

--- ArabErp.Domain/Consumption.cs
+++ ArabErp.Domain/Consumption.cs
@@ -12,11 +12,17 @@ namespace ArabErp.Domain
         public string ConsumptionNo { get; set; }
         public DateTime ConsumptionDate { get; set; }
         public int JobCardId { get; set; }
+        public decimal TotalAmount { get; set; }
         public string SpecialRemarks { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int OrganizationId { get; set; }
         public bool isActive { get; set; }
         public List<ConsumptionItem> ConsumptionItems { get; set; }
+
+        public string JobCardDate { get; set; }
+        public string SONoDate { get; set; }
+        public string FreezerUnitName { get; set; }
+        public string BoxName { get; set; }
     }
 }
