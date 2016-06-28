035ac82ab94d65a2b0a44483c79aff6edfdbfb41
praseetha <midhugeorge>
2016-06-28 17:37:39 +0530

    Unit-Edit/Delete

--- ArabErp.Web/Controllers/UnitController.cs
+++ ArabErp.Web/Controllers/UnitController.cs
@@ -17,14 +17,34 @@ namespace ArabErp.Web.Controllers
         }
         public ActionResult Create()
         {
+            ViewBag.Title = "Create";
             return View();
         }
-        public ActionResult Save(Unit objUnit)
+         [HttpPost]
+        public ActionResult Create(Unit model)
         {
-            var repo = new UnitRepository();
-            new UnitRepository().InsertUnit(objUnit);
-            return View("Create");
+            //if (ModelState.IsValid)
+            //{
+                model.OrganizationId = 1;
+                model.CreatedDate = System.DateTime.Now;
+                model.CreatedBy = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.ServerVariables["REMOTE_ADDR"];
+
+                var repo = new UnitRepository();
+                new UnitRepository().InsertUnit(model);
+                return RedirectToAction("Create");
+            //}
+            //else
+            //{
+            //    return View(model);
+            //}
         }
+
+        //public ActionResult Save(Unit objUnit)
+        //{
+        //    var repo = new UnitRepository();
+        //    new UnitRepository().InsertUnit(objUnit);
+        //    return View("Create");
+        //}
         public ActionResult FillUnitList(int? page)
         {
             int itemsPerPage = 10;
@@ -33,5 +53,60 @@ namespace ArabErp.Web.Controllers
             var List = rep.FillUnitList();
             return PartialView("UnitListView", List);
         }
+
+
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            Unit objUnit = new UnitRepository().GetUnit(Id);
+            return View("Create", objUnit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Unit model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                model.OrganizationId = 1;
+                model.CreatedDate = System.DateTime.Now;
+                model.CreatedBy = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.ServerVariables["REMOTE_ADDR"];
+
+                var repo = new UnitRepository();
+                new UnitRepository().UpdateUnit(model);
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            ViewBag.Title = "Delete";
+            Unit objUnit = new UnitRepository().GetUnit(Id);
+            return View("Create", objUnit);
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Unit model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var repo = new UnitRepository();
+                new UnitRepository().DeleteUnit(model);
+                return RedirectToAction("Create");
+            }
+            else
+            {
+            return View(model);
+            }
+
+        }
+
     }
 }
