8ff6c1944e1c49d28f901b73bd36697cdf4f818a
Shanid <praseetha@user>
2016-06-29 10:24:08 +0530

    grn bug fixes

--- ArabErp.Domain/GRN.cs
+++ ArabErp.Domain/GRN.cs
@@ -18,15 +18,19 @@ namespace ArabErp.Domain
         public int StockPointId { get; set; }
         public string SupplierDCNoAndDate { get; set; }
         public string QuotaionNoAndDate { get; set; }
+        public string VehicleNo { get; set; }
+        public string GatePassNo { get; set; }
         public string SpecialRemarks { get; set; }
         public decimal Addition { get; set; }
         public decimal Deduction { get; set; }
-        public string AdditionRemarks { get; set; }
-        public string DeductionRemarks { get; set; }
+        public int? AdditionId { get; set; }
+        public int? DeductionId { get; set; }
+        public decimal? GrandTotal { get; set; }
         public int CurrencyId { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int OrganizationId { get; set; }
+        public int ReceivedBy { get; set; }
         public bool isDirectPurchaseGRN { get; set; }
         public List<GRNItem> Items { get; set; }
     }
