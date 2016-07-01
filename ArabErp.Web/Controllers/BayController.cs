8466526291112f718a622f82537cb4d8b88eadc3
praseetha <midhugeorge>
2016-07-01 15:39:17 +0530

    Bay-Master

---
+++ ArabErp.Web/Controllers/BayController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArabErp.Domain;
+using ArabErp.DAL;
+
+namespace ArabErp.Web.Controllers
+{
+    public class BayController : BaseController
+    {
+        // GET: Bay
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FillBayList(int? page)
+        {
+            int itemsPerPage = 10;
+            int pageNumber = page ?? 1;
+            var rep = new BayRepository();
+            var List = rep.GetBays();
+            return PartialView("BayListView", List);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Create";
+            Bay Bay = new Bay();
+            Bay.BayRefNo = new BayRepository().GetRefNo(Bay);
+            return View(Bay);
+        }
+        [HttpPost]
+        public ActionResult Create(Bay model)
+        {
+            model.OrganizationId = 1;
+            model.CreatedDate = System.DateTime.Now;
+            model.CreatedBy = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.ServerVariables["REMOTE_ADDR"];
+            var result = new BayRepository().InsertBay(model);
+
+            if (result.BayId > 0)
+            {
+                TempData["Success"] = "Added Successfully!";
+                TempData["BayRefNo"] = result.BayRefNo;
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                TempData["error"] = "Oops!!..Something Went Wrong!!";
+                TempData["BayRefNo"] = null;
+                return View("Create", model);
+            }
+        }
+
+ 
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            Bay objBay = new BayRepository().GetBay(Id);
+            return View("Create", objBay);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Bay model)
+        {
+
+            model.OrganizationId = 1;
+            model.CreatedDate = System.DateTime.Now;
+            model.CreatedBy = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.ServerVariables["REMOTE_ADDR"];
+
+
+            var result = new BayRepository().UpdateBay(model);
+
+            if (result.BayId > 0)
+            {
+                TempData["Success"] = "Updated Successfully!";
+                TempData["BayRefNo"] = result.BayRefNo;
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                TempData["error"] = "Oops!!..Something Went Wrong!!";
+                TempData["BayRefNo"] = null;
+                return View("Edit", model);
+            }
+
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            ViewBag.Title = "Delete";
+            Bay objBay = new BayRepository().GetBay(Id);
+            return View("Create", objBay);
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Bay model)
+        {
+            int result = new BayRepository().DeleteBay(model);
+
+            if (result == 0)
+            {
+                TempData["Success"] = "Deleted Successfully!";
+                TempData["BayRefNo"] = model.BayRefNo;
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                if (result == 1)
+                {
+                    TempData["error"] = "Sorry!! You Cannot Delete This Bay It Is Already In Use";
+                    TempData["BayRefNo"] = null;
+                }
+                else
+                {
+                    TempData["error"] = "Oops!!..Something Went Wrong!!";
+                    TempData["BayRefNo"] = null;
+                }
+                return RedirectToAction("Create");
+            }
+
+        }
+
+    }
+}
