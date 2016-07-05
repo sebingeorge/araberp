6edb29ce54129a943fd6842903b830306c7f86b2
Sreekanth <Sreekanth@SreekanthPC>
2016-07-05 15:09:01 +0530

    menu bundle

--- ArabErp.Web/App_Start/BundleConfig.cs
+++ ArabErp.Web/App_Start/BundleConfig.cs
@@ -11,9 +11,16 @@ namespace ArabErp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/toasterScript").Include(
+                "~/Content/toastr/build/toastr.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/toasterStyle").Include(
+                "~/Content/toastr/build/toastr.min.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/menu").Include(
+                        "~/Scripts/script.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
