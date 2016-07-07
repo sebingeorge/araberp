679b856783ca5c40eb870d5d1314df1b3522c83a
Sreekanth <Sreekanth@SreekanthPC>
2016-07-07 12:08:07 +0530

    Quotation request approval setings

---
+++ ArabErp.DAL/QuotationApprovalRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+using System.Data;
+using System.Collections;
+
+namespace ArabErp.DAL
+{
+    public class QuotationApprovalRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("arab");
+        public IEnumerable<QuotationApprovalAmountSettings> GetApprovalAmountSettings()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = "select ApprovalCode, AmountFrom, AmountTo from QuotationApprovalAmountSettings";
+
+                return connection.Query<QuotationApprovalAmountSettings>(sql);
+            }
+        }
+        public IEnumerable<QuotationApprovalSettings> GetApprovalSettings()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select U.UserId, U.UserName, Q.Approval1, Q.Approval2, Q.Approval3 from 
+                            QuotationApprovalSettings Q
+                            right join [User] U on Q.UserId = U.UserId";
+
+                return connection.Query<QuotationApprovalSettings>(sql);
+            }
+        }
+        public void UpdateSettings(QuotationApprovalViewModel model)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = string.Empty;
+                foreach(var item in model.QuotationApprovalAmountSettings)
+                {
+                    sql += @"update QuotationApprovalAmountSettings set AmountFrom = '" + item.AmountFrom.ToString("#0.#0") + "', AmountTo = '" + item.AmountTo.ToString("#0.#0") + "' where ApprovalCode = " + item.ApprovalCode + ";";
+                }
+
+                connection.Query(sql);
+
+                sql = string.Empty;
+
+                sql = "delete from QuotationApprovalSettings;";
+                connection.Query(sql);
+                foreach (var item in model.QuotationApprovalSettings)
+                {
+                    sql = @"insert into QuotationApprovalSettings(UserId, Approval1, Approval2, Approval3) values
+                           (@UserId, @Approval1, @Approval2, @Approval3);";
+                    connection.Query(sql, item);
+                }
+            }
+        }
+    }
+}
