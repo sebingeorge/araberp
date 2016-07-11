ab5cb3ac97b7af1890e3af17a2c61b681e7c9979
sebin <sebin@SEBINSPC>
2016-07-11 18:26:17 +0530

    Querysheet  controller

---
+++ ArabErp.Web/Controllers/QuerySheetController.cs
@@ -0,0 +1,28 @@
+﻿using ArabErp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class QuerySheetController : BaseController
+    {
+        // GET: QuerySheet
+        public ActionResult CreateQuerySheet()
+        {
+            QuerySheet qs = new QuerySheet();
+
+            return View(qs);
+        }
+
+        [HttpPost]
+        public ActionResult CreateQuerySheet(QuerySheet qs)
+        {
+
+            qs.ProjectName = "Saved";
+            return View(qs);
+        }
+    }
+}
