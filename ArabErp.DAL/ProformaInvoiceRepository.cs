373675c6fb6644322e1ed8430f96a0004a73d602
midhu <midhugeorge>
2016-07-14 18:20:25 +0530

    proforma inv

--- ArabErp.DAL/ProformaInvoiceRepository.cs
+++ ArabErp.DAL/ProformaInvoiceRepository.cs
@@ -11,12 +11,30 @@ namespace ArabErp.DAL
     public class ProformaInvoiceRepository : BaseRepository
  {
      static string dataConnection = GetConnectionString("arab");
+
+     public IEnumerable<PendingSO> GetSaleOrdersForPerforma(int isProjectBased)
+     {
+         using (IDbConnection connection = OpenConnection(dataConnection))
+         {
+             string query = @"SELECT  DISTINCT t.SaleOrderId,SO.CustomerOrderRef,SO.SaleOrderRefNo,SO.SaleOrderDate,SO.EDateArrival,SO.EDateDelivery,SO.CustomerId,C.CustomerName,STUFF((SELECT ', ' + CAST(W.WorkDescr AS VARCHAR(10)) [text()]
+                             FROM SaleOrderItem SI inner join WorkDescription W on W.WorkDescriptionId=SI.WorkDescriptionId
+                             WHERE SI.SaleOrderId = t.SaleOrderId
+                             FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)'),1,2,' ') WorkDescription,DATEDIFF(dd,SO.SaleOrderDate,GETDATE ()) Ageing,DATEDIFF(dd,GETDATE (),SO.EDateDelivery)Remaindays,
+                             SO.TotalAmount
+                             FROM SaleOrderItem t INNER JOIN SaleOrder SO on t.SaleOrderId=SO.SaleOrderId INNER JOIN Customer C ON SO.CustomerId =C.CustomerId
+                             left join SalesInvoice SI on SO.SaleOrderId=SI.SaleOrderId 
+                             WHERE SI.SaleOrderId is null and SO.isActive=1 and SO.SaleOrderApproveStatus=1  AND SO.isProjectBased = " + isProjectBased.ToString() + @" order by SO.SaleOrderDate ASC";
+
+             return connection.Query<PendingSO>(query);
+         }
+     }
+
      public ProformaInvoice GetSaleOrderForPorforma(int SaleOrderId)
      {
          using (IDbConnection connection = OpenConnection(dataConnection))
          {
              string sql = @"SELECT  SO.SaleOrderId,SO.CustomerOrderRef,SO.SaleOrderRefNo,SO.EDateArrival,SO.EDateDelivery,SO.CustomerId,C.CustomerName,DoorNo +','+ Street+','+State CustomerAddress,SO.SaleOrderDate SaleOrderDate,
-                                SO.SaleOrderRefNo +','+ Replace(Convert(varchar,SaleOrderDate,106),' ','/') SaleOrderRefNo
+                                SO.SaleOrderRefNo +','+ Replace(Convert(varchar,SaleOrderDate,106),' ','/') SaleOrderRefNo,SO.isProjectBased
                                 FROM  SaleOrder SO  INNER JOIN Customer C  ON SO.CustomerId =C.CustomerId
                                 WHERE SO.SaleOrderId =@SaleOrderId";
              var objSaleOrders = connection.Query<ProformaInvoice>(sql, new { SaleOrderId = SaleOrderId }).Single<ProformaInvoice>();
@@ -35,6 +53,43 @@ namespace ArabErp.DAL
                             WHERE S.SaleOrderId=@SaleOrderId";
              return connection.Query<ProformaInvoiceItem>(query,
              new { SaleOrderId = SaleOrderId }).ToList();
+         }
+     }
+     public string InsertProformaInvoice(ProformaInvoice objProInvoice)
+     {
+         using (IDbConnection connection = OpenConnection(dataConnection))
+         {
+             IDbTransaction txn = connection.BeginTransaction();
+             try
+             {
+                 int internalId = DatabaseCommonRepository.GetInternalIDFromDatabase(connection, txn, typeof(ProformaInvoice).Name, "0", 1);
+
+                 objProInvoice.ProformaInvoiceRefNo = "PINV/" + internalId;
+                
+                 string sql = @"
+                    insert  into SaleOrder(ProformaInvoiceRefNo,ProformaInvoiceDate,SaleOrderId,SpecialRemarks,PaymentTerms,CreatedBy,CreatedDate,OrganizationId)
+                                   Values (@ProformaInvoiceRefNo,@ProformaInvoiceDate,@SaleOrderId,@SpecialRemarks,@PaymentTerms,@CreatedBy,@CreatedDate,@OrganizationId);
+                    SELECT CAST(SCOPE_IDENTITY() as int) ProformaInvoiceId";
+
+
+
+                 var id = connection.Query<int>(sql, objProInvoice, txn).Single();
+
+                 foreach (ProformaInvoiceItem item in objProInvoice.Items)
+                 {
+                     item.ProformaInvoiceId = id;
+                     new ProformaInvoiceItemRepository().InsertProformaInvoiceItem(item, connection, txn);
+                 }
+
+                 txn.Commit();
+
+                 return id + "|PINV/" + internalId;
+             }
+             catch (Exception)
+             {
+                 txn.Rollback();
+                 return "0";
+             }
          }
      }
    
--- ArabErp.Web/Controllers/ProformaInvoiceController.cs
+++ ArabErp.Web/Controllers/ProformaInvoiceController.cs
@@ -17,10 +17,10 @@ namespace ArabErp.Web.Controllers
         {
             return View();
         }
-        public ActionResult PendingProforma(int? page, int? isProjectBased)
+        public ActionResult PendingProforma(int? isProjectBased)
         {
-            var repo = new SaleOrderRepository();
-            IEnumerable<PendingSO> pendingSO = repo.GetSaleOrdersForHold(isProjectBased ?? 0);
+            var repo = new ProformaInvoiceRepository();
+            IEnumerable<PendingSO> pendingSO = repo.GetSaleOrdersForPerforma(isProjectBased ?? 0);
             return View(pendingSO);
         }
         public ActionResult Create(int? SaleOrderId)
@@ -55,9 +55,47 @@ namespace ArabErp.Web.Controllers
             }
 
             model.ProformaInvoiceRefNo = "PINV/" + internalId;
-            model.ProformaInvoiceDate = System.DateTime.Today;
+            model.ProformaInvoiceDate = DateTime.Now;
            
             return View(model);
         }
+          [HttpPost]
+        public ActionResult Create(ProformaInvoice model)
+        {
+            try
+            {
+                model.OrganizationId = OrganizationId;
+                model.CreatedDate = System.DateTime.Now;
+                model.CreatedBy = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.ServerVariables["REMOTE_ADDR"];
+                string id = new ProformaInvoiceRepository().InsertProformaInvoice(model);
+                if (id.Split('|')[0] != "0")
+                {
+                    TempData["success"] = "Saved successfully. Proforma Invoice No. is " + id.Split('|')[1];
+                    TempData["error"] = "";
+                    return RedirectToAction("PendingProforma", new { ProjectBased = 0 });
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (SqlException sx)
+            {
+                TempData["error"] = "Some error occured while connecting to database. Please check your network connection and try again.|" + sx.Message;
+            }
+            catch (NullReferenceException nx)
+            {
+                TempData["error"] = "Some required data was missing. Please try again.|" + nx.Message;
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Some error occured. Please try again.|" + ex.Message;
+            }
+            TempData["success"] = "";
+           
+
+            return View(model);
+        }
+
     }
 }
