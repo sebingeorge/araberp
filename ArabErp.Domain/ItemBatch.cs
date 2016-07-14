0e198066050333813a8e52caf48d6fe3e4e8feb1
sebin <sebin@SEBINSPC>
2016-07-14 13:11:10 +0530

    ItemBatch

---
+++ ArabErp.Domain/ItemBatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+   public class ItemBatch
+    {
+        public int ItemBatchId { get; set; }
+        public int GRNItemId { get; set; }
+        public int? SaleOrderItemId { get; set; }
+        public int? StoreIssueItemId { get; set; }
+        public string SerialNo { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int OrganizationId { get; set; }
+        public bool isActive { get; set; }
+    }
+}
