f44136e917507089ad966deada9a0e55d3f6cb74
praseetha <midhugeorge>
2016-07-22 10:42:59 +0530

    Customer Vs Wrok Rate

---
+++ ArabErp.Domain/CustomerVsWorkDescriptionRate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArabErp.Domain
+{
+    public class CustomerVsWorkDescriptionRate
+    {
+        public int CustomerVsWorkRateId { get; set; }
+        public int CustomerId { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? OrganizationId { get; set; }
+        public bool? isActive { get; set; }
+        public List<CustomerVsWorkRateItem> CustomerVsWorkRateItem { get; set; }
+    }
+    public class CustomerVsWorkRateItem
+    {
+        public int? SlNo { get; set; }
+        public int? WorkDescriptionId { get; set; }
+        public string WorkDescription { get; set; }
+        public decimal? FixedRate { get; set; }
+
+    }
+
+}
