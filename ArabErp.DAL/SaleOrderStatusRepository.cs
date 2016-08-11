d65d029f3fdcaa01084e7ab00c7998c25647b0ae
Sreekanth <Sreekanth@SreekanthPC>
2016-08-11 11:48:33 +0530

    sale order status report completion

--- ArabErp.DAL/SaleOrderStatusRepository.cs
+++ ArabErp.DAL/SaleOrderStatusRepository.cs
@@ -44,71 +44,66 @@ namespace ArabErp.DAL
                 //sql += " left join VehicleInPass VI on VI.VehicleInPassId = J.InPassId";
                 //sql += " order by S.SaleOrderDate, S.SaleOrderId";
 
-                sql = @"with A as (
-                select distinct S.SaleOrderId, C.CustomerName, S.SaleOrderRefNo, S.SaleOrderDate,S.EDateDelivery, '' JobCardApproval, 
-                JobCard = case when J.SaleOrderItemId is null then  '-' else JobCardNo +','+ CONVERT (VARCHAR(15),JobCardDate,106) end, 
-                JobCardComplete = case when ISNULL(JodCardCompleteStatus,0) = 1 then JobCardNo +',' + CONVERT (VARCHAR(15),JodCardCompletedDate,106) else JobCardNo +','+'-' end, 
-                WorkShopRequest = case when W.WorkShopRequestId is null then NULL else case when SIS.StoreIssueId is null then  WorkShopRequestRefNo +','+ CONVERT (VARCHAR(15),W.RequiredDate,106) else NULL end end, 
-                PurchaseRequest = case when P.PurchaseRequestId is null then NULL else  PurchaseRequestNo +','+ CONVERT (VARCHAR(15),PurchaseRequestDate,106) end, 
-                SuppyOrder =  case when GI.SupplyOrderItemId is null then NULL else case when G.GRNId is null then  SupplyOrderNo +','+ CONVERT (VARCHAR(15),SO.RequiredDate,106) else NULL end end, 
-                GRN = case when G.GRNId is null then NULL else GRNNo +','+ CONVERT (VARCHAR(15),GRNDate,106)  end, 
-                SalesInvoice = case when SLI.SalesInvoiceId is null then 'No' else 'Yes' end, 
-                VehicleInpass =  case when J.InPassId is null then NULL else  VehicleInPassNo +','+ CONVERT (VARCHAR(15),VehicleInPassDate,106)  end, 
-                VI.RegistrationNo, V.VehicleModelName,
-                Allocation = case when IB.SerialNo is null then NULL else IB.SerialNo end
-                from SaleOrderItem SI inner join SaleOrder S on S.SaleOrderId = SI.SaleOrderId 
-                inner join Customer C on C.CustomerId = S.CustomerId 
-                inner join VehicleModel V on V.VehicleModelId = SI.VehicleModelId 
-                left join WorkShopRequest W on W.SaleOrderId = S.SaleOrderId 
-                left join PurchaseRequest P on P.WorkShopRequestId = W.WorkShopRequestId 
-                left join PurchaseRequestItem PRI on PRI.PurchaseRequestId	= P.PurchaseRequestId 
-                left join JobCard J on J.SaleOrderItemId = SI.SaleOrderItemId 
-                left join SupplyOrderItem SUI on SUI.PurchaseRequestItemId = PRI.PurchaseRequestItemId 
-                left join SupplyOrder SO on SO.SupplyOrderId = SUI.SupplyOrderId 
-                left join GRNItem GI on GI.SupplyOrderItemId = SUI.SupplyOrderItemId 
-                left join GRN G on G.GRNId = G.GRNId 
-                left join SalesInvoiceItem SLI on SLI.SaleOrderItemId = SI.SaleOrderItemId 
-                left join StoreIssue SIS on SIS.WorkShopRequestId=W.WorkShopRequestId 
-                left join VehicleInPass VI on VI.VehicleInPassId = J.InPassId 
-                left join ItemBatch IB on IB.SaleOrderItemId = SI.SaleOrderItemId)
-                select 
-                distinct SaleOrderId, CustomerName, SaleOrderRefNo, SaleOrderDate, EDateDelivery,VehicleModelName,
-                RegistrationNo, VehicleInPass, 
-                JobCard = (STUFF((SELECT ',' + CAST(T.JobCard AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                sql = @"select distinct SI.SaleOrderItemId, S.SaleOrderId, C.CustomerName, S.SaleOrderRefNo, S.SaleOrderDate,S.EDateDelivery, 
+                        VI.RegistrationNo, V.VehicleModelName,
+                        VehicleInpass =  case when VI.VehicleInPassId is null then NULL else  VehicleInPassNo +','+ CONVERT (VARCHAR(15),VehicleInPassDate,106)  end, 
+                        JobCard = cast('' as varchar(max)), 
+                        JobCardComplete = cast('' as varchar(max)), 
+                        WorkShopRequest = cast('' as varchar(max)), 
+                        PurchaseRequest = cast('' as varchar(max)), 
+                        SuppyOrder =  cast('' as varchar(max)), 
+                        GRN = cast('' as varchar(max)), 
+                        SalesInvoice = case when SLI.SalesInvoiceId is null then NULL else SLIH.SalesInvoiceRefNo + ',' + CONVERT (VARCHAR(15),SLIH.SalesInvoiceDate,106) end, 
+                        Allocation = case when IB.SerialNo is null then NULL else IB.SerialNo end
+                        into #RESULT
+                        from SaleOrderItem SI inner join SaleOrder S on S.SaleOrderId = SI.SaleOrderId 
+                        inner join Customer C on C.CustomerId = S.CustomerId 
+                        inner join VehicleModel V on V.VehicleModelId = SI.VehicleModelId 
+                        --left join WorkShopRequest W on W.SaleOrderId = S.SaleOrderId 
+                        --left join PurchaseRequest P on P.WorkShopRequestId = W.WorkShopRequestId 
+                        --left join PurchaseRequestItem PRI on PRI.PurchaseRequestId	= P.PurchaseRequestId 
+                        --left join JobCard J on J.SaleOrderItemId = SI.SaleOrderItemId 
+                        --left join SupplyOrderItem SUI on SUI.PurchaseRequestItemId = PRI.PurchaseRequestItemId 
+                        --left join SupplyOrder SO on SO.SupplyOrderId = SUI.SupplyOrderId 
+                        --left join GRNItem GI on GI.SupplyOrderItemId = SUI.SupplyOrderItemId 
+                        --left join GRN G on G.GRNId = G.GRNId 
+                        left join SalesInvoiceItem SLI on SLI.SaleOrderItemId = SI.SaleOrderItemId 
+                        left Join SalesInvoice SLIH on SLIH.SalesInvoiceId = SLI.SalesInvoiceId
+                        --left join StoreIssue SIS on SIS.WorkShopRequestId=W.WorkShopRequestId 
+                        left join VehicleInPass VI on VI.SaleOrderItemId = SI.SaleOrderItemId
+                        left join ItemBatch IB on IB.SaleOrderItemId = SI.SaleOrderItemId;
 
-                JobCardComplete = (STUFF((SELECT ',' + CAST(T.JobCardComplete AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        --select A.SaleOrderItemId, J.JobCardNo, J.JobCardDate from #RESULT A left join JobCard J on J.SaleOrderItemId = A.SaleOrderItemId
+                        --where J.SaleOrderItemId is not null
 
-                WorkShopRequest = (STUFF((SELECT ',' + CAST(T.WorkShopRequest AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        update R set R.JobCard = J.JobCardNo +', '+ CONVERT (VARCHAR(15),J.JobCardDate,106) 
+                        from JobCard J, #RESULT R
+                        where J.SaleOrderId = R.SaleOrderId and R.SaleOrderItemId = J.SaleOrderItemId;
 
-                PurchaseRequest = (STUFF((SELECT ',' + CAST(T.PurchaseRequest AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        update R set R.JobCardComplete = J.JobCardNo +', '+ CONVERT (VARCHAR(15),J.JodCardCompletedDate,106) 
+                        from JobCard J, #RESULT R
+                        where J.SaleOrderId = R.SaleOrderId and R.SaleOrderItemId = J.SaleOrderItemId
+                        and J.JodCardCompleteStatus = 1;
 
-                SuppyOrder = (STUFF((SELECT ',' + CAST(T.SuppyOrder AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        update #RESULT set WorkShopRequest = (STUFF((SELECT ', ' + CAST(T.WorkShopRequestRefNo AS VARCHAR(MAX))
+                        FROM WorkShopRequest T where T.SaleOrderId = #RESULT.SaleOrderId
+                        FOR XML PATH('')),1,1,''))
 
-                GRN = (STUFF((SELECT ',' + CAST(T.GRN AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        update #RESULT set PurchaseRequest = (STUFF((SELECT ', ' + CAST(P.PurchaseRequestNo AS VARCHAR(MAX))
+                        FROM PurchaseRequest P inner join WorkShopRequest W on P.WorkShopRequestId = W.WorkShopRequestId where W.SaleOrderId = #RESULT.SaleOrderId
+                        FOR XML PATH('')),1,1,''))
 
-                Allocation = (STUFF((SELECT ',' + CAST(T.Allocation AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,'')),
+                        update #RESULT set SuppyOrder = (STUFF((SELECT distinct ', ' + CAST(SI.SupplyOrderNo AS VARCHAR(MAX))
+                        FROM PurchaseRequest P inner join WorkShopRequest W on P.WorkShopRequestId = W.WorkShopRequestId 
+                        inner join PurchaseRequestItem PRI on PRI.PurchaseRequestId = P.PurchaseRequestId
+                        inner join SupplyOrderItem SUI on SUI.PurchaseRequestItemId = PRI.PurchaseRequestItemId
+                        inner join SupplyOrder SI on SI.SupplyOrderId = SUI.SupplyOrderId
+                        where W.SaleOrderId = #RESULT.SaleOrderId
+                        FOR XML PATH('')),1,1,''))
 
-                SalesInvoice = (STUFF((SELECT ',' + CAST(T.SalesInvoice AS VARCHAR(MAX))
-                FROM A T where T.SaleOrderId = A1.SaleOrderId and T.VehicleModelName = A1.VehicleModelName and T.RegistrationNo = A1.RegistrationNo
-                FOR XML PATH('')),1,1,''))
+                        select * from #RESULT;
 
-                from A A1
-                where SalesInvoice = 'No'
-                order by SaleOrderDate, SaleOrderId";
+                        drop table #RESULT;";
 
                 return connection.Query<SaleOrderStatus>(sql);
             }
