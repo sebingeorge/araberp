bfdb9e1b323763dd8ddb7c19dffcff4068a23aaf
sebin <sebin@SEBINSPC>
2016-08-17 17:36:56 +0530

    ProductionPLan Controller Added

---
+++ ArabErp.Web/Controllers/ProductionPlanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class ProductionPlanController : Controller
+    {
+        // GET: ProductionPlan
+        public ActionResult CreateProductionPlan()
+        {
+            return View();
+        }
+    }
+}
