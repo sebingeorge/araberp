bd56f1f1e69fb336ca953bcb32227f9414e54ae6
praseetha <midhugeorge>
2016-08-19 15:14:44 +0530

    Supply Order summary-Report

--- ArabErp.Web/Controllers/SupplyOrderSummaryController.cs
+++ ArabErp.Web/Controllers/SupplyOrderSummaryController.cs
@@ -10,13 +10,12 @@ using System.Data.SqlClient;
 
 namespace ArabErp.Web.Controllers
 {
-    public class SupplyOrderRegisterController : BaseController
+    public class SupplyOrderSummaryController : BaseController
     {
-        // GET: SupplyOrderRegister
+        // GET: SupplyOrderSummary
         public ActionResult Index()
         {
             FillSupplier();
-            FillItem();
             return View();
         }
         public void FillSupplier()
@@ -26,18 +25,12 @@ namespace ArabErp.Web.Controllers
             ViewBag.SupplierList = new SelectList(result, "Id", "Name");
 
         }
-        public void FillItem()
-        {
-            DropdownRepository repo=new DropdownRepository();
-            var result = repo.SOItemDropdown();
-            ViewBag.ItemList = new SelectList(result, "Id", "Name");
-        }
 
-        public ActionResult SupplyOrderRegister(DateTime? from, DateTime? to, int id = 0, int itmid = 0)
+        public ActionResult SupplyOrderSummary(DateTime? from, DateTime? to, int id = 0, int itmid = 0)
         {
             from = from ?? DateTime.Today.AddMonths(-7);
             to = to ?? DateTime.Today;
-            return PartialView("_SupplyOrderRegister", new SupplyOrderRegisterRepository().GetSupplyOrderRegisterData(from, to, id, itmid, OrganizationId));
+            return PartialView("_SupplyOrderSummary", new SupplyOrderRegisterRepository().GetSupplyOrderRegisterData(from, to, id, itmid, OrganizationId));
         }
 
     }
