c5fb9e752e5c3a3222fc715342a02659ea1e1bce
praseetha <midhugeorge>
2016-08-20 10:22:47 +0530

    Pending PAR -Register

---
+++ ArabErp.Web/Controllers/PendingPARregisterController.cs
@@ -0,0 +1,23 @@
+﻿using ArabErp.DAL;
+using ArabErp.Domain;
+using ArabErp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+
+namespace ArabErp.Web.Controllers
+{
+    public class PendingPARregisterController : BaseController
+    {
+        // GET: PendingPARregister
+        public ActionResult Index()
+        {
+            return View(new PurchaseRequestRepository().GetPendingPARregisterData(OrganizationId));
+            //return View();
+        }
+    }
+}
