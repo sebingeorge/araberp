1842ef87e7025835a0171ab3396cb9845a1c1287
Sreekanth <Sreekanth@SreekanthPC>
2016-08-22 11:03:24 +0530

    Prefix settings

---
+++ ArabErp.Web/Controllers/PrefixSettingsController.cs
@@ -0,0 +1,19 @@
+﻿using ArabErp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabErp.Web.Controllers
+{
+    public class PrefixSettingsController : BaseController
+    {
+        // GET: PrefixSettings
+        public ActionResult Index()
+        {
+            PrefixSettingsRepository repo = new PrefixSettingsRepository();
+            return View(repo.GetPrefixSettings(OrganizationId));
+        }
+    }
+}
