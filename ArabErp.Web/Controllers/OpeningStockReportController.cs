5d9b21e1f9ef237866c9b1d918b4cae39152243b
praseetha <midhugeorge>
2016-08-24 11:56:39 +0530

    Opening Stock -Report

---
+++ ArabErp.Web/Controllers/OpeningStockReportController.cs
@@ -0,0 +1,63 @@
+﻿using ArabErp.DAL;
+using ArabErp.Domain;
+using ArabErp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace ArabErp.Web.Controllers
+{
+    public class OpeningStockReportController : BaseController
+    {
+        // GET: OpeningStockReport
+        public ActionResult Index()
+        {
+            InitDropdown();
+            FillWarehouse();
+            FillItemCategory();
+            OpeningStockReport os = new OpeningStockReport();
+            os.itmCatId = 0;
+            return View("Index", os);
+        }
+
+        public void InitDropdown()
+        {
+            var List = "";
+            ViewBag.ItemList = new SelectList(List, "Id", "Name");
+
+        }
+        public void FillWarehouse()
+        {
+            DropdownRepository repo = new DropdownRepository();
+            var result = repo.StockpointDropdown();
+            ViewBag.WarehouseList = new SelectList(result, "Id", "Name");
+        }
+        public void FillItemCategory()
+        {
+            DropdownRepository repo = new DropdownRepository();
+            var result = repo.ItemCategoryDropdown();
+            ViewBag.ItemCatList = new SelectList(result, "Id", "Name");
+        }
+        public void FillItem(int Id)
+        {
+            DropdownRepository repo = new DropdownRepository();
+            var result = repo.ItemCatDropdown(Id);
+            ViewBag.ItemList = new SelectList(result, "Id", "Name");
+        }
+
+        public ActionResult OpeningStockRegister( int stkid = 0, int itmcatid = 0, int itmid = 0)
+        {
+         
+            return PartialView("_OpeningStockRegister", new OpeningStockRepository().GetClosingStockData(stkid, itmcatid, itmid, OrganizationId));
+        }
+    
+        public ActionResult Item(int Code)
+        {
+            FillItem(Code);
+            return PartialView("_ItemDropDown");
+        }
+    }
+}
