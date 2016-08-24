0ccaba4ce77b6a33cbe4634af73fe4e3a2d63df9
praseetha <midhugeorge>
2016-08-24 15:20:19 +0530

    Current Stock-Report

--- ArabErp.DAL/ClosingStockRepository.cs
+++ ArabErp.DAL/ClosingStockRepository.cs
@@ -33,5 +33,18 @@ namespace ArabErp.DAL
                 return connection.Query<ClosingStock>(qry, new { stkid = stockPointId, itmcatid = itemCategoryId, itmid = itemId, OrganizationId = OrganizationId, Ason = asOn }).ToList();
             }
         }
+
+        public IEnumerable<ClosingStock> GetCurrentStockData(int stockPointId, int itemCategoryId, int itemId, int OrganizationId)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+
+                string qry = @"select ItemName,SUM(Quantity)Quantity,UnitName from StockUpdate SU INNER JOIN Item I ON I.ItemId=SU.ItemId
+                               inner join Unit U ON U.UnitId=I.ItemUnitId
+                               where  SU.ItemId = ISNULL(NULLIF(@itmid, 0), SU.ItemId) and I.ItemCategoryId=ISNULL(NULLIF(@itmcatid, 0), I.ItemCategoryId) and SU.OrganizationId=@OrganizationId AND SU.StockPointId = ISNULL(NULLIF(@stkid, 0), SU.StockPointId)
+                               GROUP BY ItemName,UnitName";
+                return connection.Query<ClosingStock>(qry, new { stkid = stockPointId, itmcatid = itemCategoryId, itmid = itemId, OrganizationId = OrganizationId}).ToList();
+            }
+        }
     }
 }
