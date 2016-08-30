d274cbb6c91e0a16eb7503943a6e39a9b414b513
shanid <vivelashani>
2016-08-30 14:59:33 +0530

    stock transfer

---
+++ ArabErp.Domain/StockTransferItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+    public class StockTransferItem
+    {
+        public int StockTransferItemId { get; set; }
+        public int StockTransferId { get; set; }
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public bool isActive { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
