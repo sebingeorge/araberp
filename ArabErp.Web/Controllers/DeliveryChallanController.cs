02d291c63aa9b53255aefebc882fe7910c2aab2b
praseetha <midhugeorge>
2016-09-15 10:09:34 +0530

    DeliveryChallan-Modifications

--- ArabErp.Web/Controllers/DeliveryChallanController.cs
+++ ArabErp.Web/Controllers/DeliveryChallanController.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CrystalDecisions.CrystalReports.Engine;
+using System.IO;
+using ArabErp.Web.Models;
+using System.Data;
 
 namespace ArabErp.Web.Controllers
 {
@@ -108,5 +112,94 @@ namespace ArabErp.Web.Controllers
         {
             return PartialView("_PreviousList", new DeliveryChallanRepository().GetAllDeliveryChallan(id, cusid, OrganizationId, from, to));
         }
+
+        public ActionResult Print(int Id)
+        {
+
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "DeliveryChallan.rpt"));
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add("Head");
+            ds.Tables.Add("Items");
+
+            //-------HEAD
+            ds.Tables["Head"].Columns.Add("DeliveryChallanRefNo");
+            ds.Tables["Head"].Columns.Add("DeliveryChallanDate");
+            ds.Tables["Head"].Columns.Add("Customer");
+            ds.Tables["Head"].Columns.Add("SONoDate");
+            ds.Tables["Head"].Columns.Add("JCNoDate");
+            ds.Tables["Head"].Columns.Add("RegistrationNo");
+            ds.Tables["Head"].Columns.Add("WorkDesc");
+            ds.Tables["Head"].Columns.Add("VehicleModel");
+            ds.Tables["Head"].Columns.Add("Employee");
+            ds.Tables["Head"].Columns.Add("PaymentTerms");
+            ds.Tables["Head"].Columns.Add("SpecialRemarks");
+            //-------DT
+            ds.Tables["Items"].Columns.Add("SerialNo");
+            ds.Tables["Items"].Columns.Add("Item");
+            ds.Tables["Items"].Columns.Add("WarrantyPeriod");
+
+            DeliveryChallanRepository repo = new DeliveryChallanRepository();
+            var Head = repo.GetDeliveryChallanHD(Id);
+
+            DataRow dr = ds.Tables["Head"].NewRow();
+            dr["DeliveryChallanRefNo"] = Head.DeliveryChallanRefNo;
+            dr["DeliveryChallanDate"] = Head.DeliveryChallanDate.ToString("dd-MMM-yyyy");
+            dr["Customer"] = Head.Customer;
+            dr["SONoDate"] = Head.SONODATE;
+            dr["JCNoDate"] = Head.JobCardDate;
+            dr["RegistrationNo"] = Head.RegistrationNo;
+            dr["WorkDesc"] = Head.WorkDesc;
+            dr["VehicleModel"] = Head.VehicleModel;
+            dr["Employee"] = Head.EmployeeName;
+            dr["PaymentTerms"] = Head.PaymentTerms;
+            dr["SpecialRemarks"] = Head.SpecialRemarks;
+            ds.Tables["Head"].Rows.Add(dr);
+
+
+            JobCardTaskRepository repo1 = new JobCardTaskRepository();
+            var Items = repo1.GetJobCardDT(Id);
+            foreach (var item in Items)
+            {
+                var JCItem = new JobCardTask
+                {
+                    TaskDate = item.TaskDate,
+                    Employee = item.Employee,
+                    Description = item.Description,
+                    StartTime = item.StartTime,
+                    EndTime = item.EndTime
+                };
+
+                DataRow dri = ds.Tables["Items"].NewRow();
+                dri["TaskDate"] = JCItem.TaskDate.ToString("dd-MMM-yyyy");
+                dri["Employee"] = JCItem.Employee;
+                dri["Description"] = JCItem.Description;
+                dri["StartTime"] = JCItem.StartTime;
+                dri["EndTime"] = JCItem.EndTime;
+                ds.Tables["Items"].Rows.Add(dri);
+            }
+
+            ds.WriteXml(Path.Combine(Server.MapPath("~/XML"), "DeliveryChallan.xml"), XmlWriteMode.WriteSchema);
+
+            rd.SetDataSource(ds);
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", String.Format("DeliveryChallan{0}.pdf", Id.ToString()));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
     }
 }
