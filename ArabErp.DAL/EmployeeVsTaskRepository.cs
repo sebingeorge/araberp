b6567db72f45b5cd443d2b4fed3be118bff079a9
sebin <sebin@ihitsindia.com>
2016-09-26 18:31:31 +0530

    EmployeeVsTask Model

---
+++ ArabErp.DAL/EmployeeVsTaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ArabErp.Domain;
+using System.Data;
+
+namespace ArabErp.DAL
+{
+    public class EmployeeVsTaskRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("arab");
+    }
+
+}
---
+++ ArabErp.Domain/EmployeeVsTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+  public  class EmployeeVsTask
+    {
+    public int    EmployeeVsTaskId { get; set; }
+    public int EmployeeId                  { get; set; }
+    public string JobCardTaskName { get; set; }
+    [Required]
+    public int JobCardTaskMasterId         { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public int OrganizationId              { get; set; }
+    public int isActive { get; set; }
+
+    }
+}
