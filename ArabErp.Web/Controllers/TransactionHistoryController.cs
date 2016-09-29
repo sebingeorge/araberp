d4688e65a64897666148779bb323d1cd9b4f0d52
praseetha <midhugeorge>
2016-09-29 17:54:55 +0530

    Transaction History-Modifications

--- ArabErp.Web/Controllers/TransactionHistoryController.cs
+++ ArabErp.Web/Controllers/TransactionHistoryController.cs
@@ -12,15 +12,23 @@ namespace ArabErp.Web.Controllers
     {
         // GET: TransactionHistory
         public ActionResult Index()
-        {
-            string From = DateTime.Today.AddMonths(-1).ToString("dd-MMMM-yyyy");
-            string To = DateTime.Today.ToString("dd-MMMM-yyyy");
-            return View((new TransactionHistoryRepository().GetTransactionHistorys(From, To)));
+        {   
+            //string user = "";
+            //string form = "";
+            //string mode = "";
+
+            return View();
+            //return View((new TransactionHistoryRepository().GetTransactionHistorys(user, form, mode, From, To)));
         }
 
-        public ActionResult Grid(string FromDate, string ToDate)
+        //public ActionResult Grid(string FromDate, string ToDate)
+        public ActionResult Grid(DateTime? from, DateTime? to,string user = "", string form = "", string mode = "")
         {
-            return PartialView("_Grid",(new TransactionHistoryRepository().GetTransactionHistorys(FromDate, ToDate)));
+             from = from ?? DateTime.Today.AddMonths(-1);
+             to = to?? DateTime.Today;
+            //from = from ?? FYStartdate; ;
+            //to = to ?? DateTime.Today;
+            return PartialView("_Grid", (new TransactionHistoryRepository().GetTransactionHistorys(from, to,user.Trim(), form.Trim(), mode.Trim())).ToList());
         }
     }
 }
