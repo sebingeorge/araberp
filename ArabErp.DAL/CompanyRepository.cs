b440a2923f3628bbebfc2c3ac8d3433ff68a4b9d
ashathomas <ashathomas>
2016-10-01 09:41:19 +0530

    Company:Changes

--- ArabErp.DAL/CompanyRepository.cs
+++ ArabErp.DAL/CompanyRepository.cs
@@ -37,10 +37,10 @@ namespace ArabErp.DAL
 
                 string sql = @"insert  into mstAccCompany(cmpUsercode,cmpName,cmpShrtName,cmpRemarks,cmpDoorNo,cmpStreet,
                                                      cmpArea,cmpState,cmpPhNo,cmpFax,cmpEmail,cmpWeb,cmpPAN,cmpSSN,
-                                                     cmpVATNo,cmpCSTNo,cmpIECode,cmpWorkSheetNo,cmpFormPrefix,CreatedBy,CreatedDate) 
+                                                     cmpVATNo,cmpCSTNo,cmpIECode,cmpWorkSheetNo,cmpFormPrefix,CreatedBy,CreatedDate,IsActive) 
                                                      Values (@cmpUsercode,@cmpName,@cmpShrtName,@cmpRemarks,@cmpDoorNo,
                                                      @cmpStreet,@cmpArea,@cmpState,@cmpPhNo,@cmpFax,@cmpEmail,@cmpWeb,@cmpPAN,
-                                                     @cmpSSN,@cmpVATNo,@cmpCSTNo,@cmpIECode,@cmpWorkSheetNo,@cmpFormPrefix,@CreatedBy,@CreatedDate);
+                                                     @cmpSSN,@cmpVATNo,@cmpCSTNo,@cmpIECode,@cmpWorkSheetNo,@cmpFormPrefix,@CreatedBy,@CreatedDate,1);
                                                      SELECT CAST(SCOPE_IDENTITY() as int)";
 
                 try
@@ -64,8 +64,81 @@ namespace ArabErp.DAL
                 return objCompany;
             }
         }
+        public IEnumerable<Company> CompanyList()
+        {
 
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                return connection.Query<Company>(" select cmpCode,cmpUsercode,cmpName,cmpShrtName,cmpDoorNo,cmpStreet,cmpArea from mstAccCompany where IsActive=1 ").ToList();
+            }
+        }
 
+        public Company UpdateCompany(Company objCompany)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @" UPDATE mstAccCompany SET cmpUsercode=@cmpUsercode ,cmpName=@cmpName,cmpShrtName=@cmpShrtName,cmpRemarks=@cmpRemarks,cmpDoorNo=@cmpDoorNo,cmpStreet=@cmpStreet,cmpArea=@cmpArea,
+                                cmpState=@cmpState,cmpPhNo=@cmpPhNo,cmpFax=@cmpFax,cmpEmail=@cmpEmail,cmpWeb=@cmpWeb,cmpPAN=@cmpPAN,cmpSSN=@cmpSSN,
+                                cmpVATNo=@cmpVATNo,cmpCSTNo=@cmpCSTNo,cmpIECode=@cmpIECode,cmpWorkSheetNo=@cmpWorkSheetNo,cmpFormPrefix=@cmpFormPrefix,CreatedBy=@CreatedBy,CreatedDate =@CreatedDate
+                                WHERE cmpcode = @cmpcode";
+
+                var id = connection.Execute(sql, objCompany);
+                InsertLoginHistory(dataConnection, objCompany.CreatedBy, "Update", "mstAccCompany", id.ToString(), "0");
+                return objCompany;
+            }
+        }
+        public Company GetCompany(int cmpcode)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from mstAccCompany
+                        where cmpcode=@cmpcode ";
+
+                var objCompany = connection.Query<Company>(sql, new
+                {
+                    cmpcode = cmpcode
+                }).First<Company>();
+
+                return objCompany;
+            }
+        }
+
+        public int DeleteCustomer(Company objCompany)
+        {
+            int result = 0;
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @" UPDATE mstAccCompany SET isActive=0  WHERE cmpcode=@cmpcode";
+                try
+                {
+
+                    var id = connection.Execute(sql, objCompany);
+                    objCompany.cmpCode = id;
+                    result = 0;
+                    InsertLoginHistory(dataConnection, objCompany.CreatedBy, "Delete", "mstAccCompany", id.ToString(), "0");
+                }
+                catch (SqlException ex)
+                {
+                    int err = ex.Errors.Count;
+                    if (ex.Errors.Count > 0) // Assume the interesting stuff is in the first error
+                    {
+                        switch (ex.Errors[0].Number)
+                        {
+                            case 547: // Foreign Key violation
+                                result = 1;
+                                break;
+
+                            default:
+                                result = 2;
+                                break;
+                        }
+                    }
+
+                }
+
+                return result;
+            }
+        }
 
     }
 }
--- ArabErp.Web/Controllers/CompanyController.cs
+++ ArabErp.Web/Controllers/CompanyController.cs
@@ -53,9 +53,94 @@ namespace ArabErp.Web.Controllers
              //   TempData["CustomerRefNo"] = null;
                 return View("Create", model);
             }
-
-
-       
         }
+        public ActionResult CompanyList(int? page)
+        {
+            int itemsPerPage = 10;
+            int pageNumber = page ?? 1;
+            var repo = new CompanyRepository();
+            var List = repo.CompanyList();
+            return PartialView("CompanyListView", List);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            Company objCompany = new CompanyRepository().GetCompany(Id);
+           
+            return View("Create", objCompany);
+        }
+        [HttpPost]
+        public ActionResult Edit(Company model)
+        {
+            model.CreatedDate = System.DateTime.Now;
+            model.CreatedBy = UserID.ToString();
+            var repo = new CompanyRepository();
+            bool isexists = repo.IsFieldExists(repo.ConnectionString(), "mstAccCompany", "cmpUsercode", model.cmpUsercode, "cmpCode", model.cmpCode);
+            if (!isexists)
+            {
+                var result = new CompanyRepository().UpdateCompany(model);
+                if (result.cmpCode > 0)
+                {
+
+                    TempData["Success"] = "Updated Successfully!";
+                   // TempData["CustomerRefNo"] = result.CustomerRefNo;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                   
+                    TempData["error"] = "Oops!!..Something Went Wrong!!";
+                    //TempData["CustomerRefNo"] = null;
+                    return View("Create", model);
+                }
+            }
+            else
+            {
+                TempData["error"] = "This Name Alredy Exists!!";
+               // TempData["CustomerRefNo"] = null;
+                return View("Create", model);
+            } 
+        }
+        public ActionResult Delete(int Id)
+        {
+             ViewBag.Title = "Delete";
+             Company objCompany = new CompanyRepository().GetCompany(Id);
+             return View("Create", objCompany);
+
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Delete(Company model)
+        {
+            int result = new CompanyRepository().DeleteCustomer(model);
+
+            if (result == 0)
+            {
+                TempData["Success"] = "Deleted Successfully!";
+              //  TempData["CustomerRefNo"] = model.CustomerRefNo;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                if (result == 1)
+                {
+                    TempData["error"] = "Sorry!! You Cannot Delete This Customer. It Is Already In Use";
+              //      TempData["CustomerRefNo"] = null;
+                }
+                else
+                {
+                    TempData["error"] = "Oops!!..Something Went Wrong!!";
+               //     TempData["CustomerRefNo"] = null;
+                }
+                return RedirectToAction("Index");
+            }
+
+        }
+
+    
+    
     }
 }
