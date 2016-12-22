3581f95ad2b4b784f1ffaeebba31218f76c47faa
Midhu George <MidhuGeorge@gmail.com>
2016-12-22 17:36:33 +0530

    rate setting

---
+++ ArabErp.Domain/StandardSellingRate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabErp.Domain
+{
+   public class StandardSellingRate
+    {
+       
+        public List<StandardSellingRateItem> StandardSellingRateItems { get; set; }
+
+    }
+   public class StandardSellingRateItem
+   {
+       public int ItemId { get; set; }
+       [Required]
+       public string ItemName { get; set; }
+       public string PartNo { get; set; }
+       public decimal? Rate { get; set; }
+   }
+}
