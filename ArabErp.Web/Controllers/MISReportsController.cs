a399bf1d9691f3f4605bdb9e39b75d6df1237ef9
ashathomas <ashathomas>
2016-12-28 11:24:27 +0530

    dcreport

--- ArabErp.Web/Controllers/MISReportsController.cs
+++ ArabErp.Web/Controllers/MISReportsController.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArabErp.Domain;
+using System.Collections;
+using CrystalDecisions.CrystalReports.Engine;
+using System.IO;
+using ArabErp.Web.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ArabErp.Web.Controllers
 {
@@ -17,13 +24,21 @@ namespace ArabErp.Web.Controllers
 
         public ActionResult DCReport()
         {
+            FillNeworService();
             ViewBag.Year = FYStartdate.Year;
             return View();
         }
 
-        public ActionResult DCReportGrid(int? month, int? year, string ChassisNo = "", string UnitSlNo = "", string Customer = "", string JobcardNo = "", string Installation="")
+        public ActionResult DCReportGrid(int? month, int? year, string ChassisNo = "", string UnitSlNo = "", string Customer = "", string JobcardNo = "", string InstallType = "")
         {
-            return PartialView("_DCReportGrid", new MISReportsRepository().GetDCReport(OrganizationId, month, year, ChassisNo, UnitSlNo, Customer, JobcardNo, Installation));
+            return PartialView("_DCReportGrid", new MISReportsRepository().GetDCReport(OrganizationId, month, year, ChassisNo, UnitSlNo, Customer, JobcardNo, InstallType));
+        }
+        public void FillNeworService()
+        {
+            List<Dropdown> types = new List<Dropdown>();
+            types.Add(new Dropdown { Id = 1, Name = "New Installation" });
+            types.Add(new Dropdown { Id = 2, Name = "Service" });
+            ViewBag.Type = new SelectList(types, "Id", "Name");
         }
     }
 }
