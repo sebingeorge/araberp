53c824b55bafc2979cf941e0d569ca1fa9942054
Praseetha Kuriakose <vivelashani>
2016-12-30 17:03:05 +0530

    IndentVSInpassVsGRN

---
+++ ArabErp.DAL/IndentVsSOVsInpassStatusRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using ArabErp.Domain;
+using System.Data;
+
+namespace ArabErp.DAL
+{
+    public class IndentVsSOVsInpassStatusRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("arab");
+        public IEnumerable<IndentVsSOVsInpassStatus> GetGRNRegister()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = string.Empty;
+
+                sql = @"SELECT S.SupplierName Supplier,IG.ItemGroupName ItemGroup,I.ItemName Item,
+                        P.PurchaseRequestNo IndentNo,Convert(varchar(50),P.PurchaseRequestDate,106) IndenDate,PI.Quantity IndentQty,PRU.UserName IndentUser,
+                        SO.SupplyOrderNo SONo,Convert(varchar(50),SO.SupplyOrderDate,106) SODate,SOI.OrderedQty SOQty,SOU.UserName SOUser,Convert(varchar(50),SO.RequiredDate,106) SODueDate,SOI.Rate SORate,SOI.Amount SOAmount,
+                        G.GRNNo GRNNo,Convert(varchar(50),G.GRNDate,106) GRNDate,GI.Quantity GRNQty,GU.UserName GRNUser,
+                        (SOI.OrderedQty-GI.Quantity)PendingQty
+                        FROM PurchaseRequest P
+                        INNER JOIN PurchaseRequestItem PI ON PI.PurchaseRequestId=P.PurchaseRequestId
+                        INNER JOIN Item I ON I.ItemId=PI.ItemId
+                        INNER JOIN ItemGroup IG ON IG.ItemGroupId=I.ItemGroupId
+                        INNER JOIN [User] PRU ON PRU.UserId=P.CreatedBy
+                        LEFT JOIN SupplyOrderItem SOI ON SOI.PurchaseRequestItemId=PI.PurchaseRequestItemId
+                        LEFT JOIN SupplyOrder SO ON SO.SupplyOrderId=SOI.SupplyOrderId
+                        LEFT JOIN [User] SOU ON SOU.UserId=SO.CreatedBy
+                        LEFT JOIN Supplier S ON S.SupplierId=SO.SupplierId
+                        LEFT JOIN GRNItem GI ON GI.SupplyOrderItemId=SOI.SupplyOrderItemId AND GI.ItemId=I.ItemId
+                        LEFT JOIN GRN G ON G.GRNId=GI.GRNId
+                        LEFT JOIN [User] GU ON GU.UserId=G.CreatedBy";
+
+                return connection.Query<IndentVsSOVsInpassStatus>(sql);
+            }
+        }
+    }
+}
