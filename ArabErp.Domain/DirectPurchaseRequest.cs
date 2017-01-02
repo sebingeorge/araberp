25f60bf8a716930760a26c4305519643d4f9db5b
Midhu George <MidhuGeorge@gmail.com>
2017-01-02 10:00:40 +0530

    dll error

--- ArabErp.Domain/DirectPurchaseRequest.cs
+++ ArabErp.Domain/DirectPurchaseRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 
 namespace ArabErp.Domain
 {
