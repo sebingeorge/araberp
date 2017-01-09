537915b5a71b4e0469a3194a821816951cbbff60
ashathomas <ashathomas>
2017-01-09 18:40:52 +0530

    sales invoice

--- ArabErp.Domain/SalesInvoiceItem.cs
+++ ArabErp.Domain/SalesInvoiceItem.cs
@@ -24,6 +24,7 @@ namespace ArabErp.Domain
         public decimal? Rate { get; set; }
         public decimal? Discount { get; set; }
         public decimal? Amount { get; set; }
+        public decimal? PrintAmount { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int OrganizationId { get; set; }
