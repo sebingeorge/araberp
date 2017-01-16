d1862ae76d376d990f8c97cce154da4636088e35
ashathomas <ashathomas>
2017-01-16 09:58:37 +0530

    stock movement

--- ArabErp.DAL/StockMovementRegisterRepository.cs
+++ ArabErp.DAL/StockMovementRegisterRepository.cs
@@ -30,6 +30,19 @@ namespace ArabErp.DAL
 							GROUP BY  I.ItemId,I.ItemRefNo,I.ItemName,ItemCategoryId,U.UnitName";
 
 
+//                string qry = @"select I.ItemId,I.ItemRefNo,I.ItemName,U.UnitName,
+//                            (SELECT SUM(S.Quantity)Quantity from StockUpdate S WHERE StockInOut='IN' AND S.ItemId=isnull(@itmid,S.ItemId) AND  S.OrganizationId=@OrganizationId AND I.ItemCategoryId=isnull(@itmcatid,I.ItemCategoryId) ) INQTY,
+//                            (SELECT SUM(S.Quantity)Quantity from StockUpdate S WHERE StockInOut='OUT' AND S.ItemId=isnull(@itmid,S.ItemId) AND  S.OrganizationId=@OrganizationId AND I.ItemCategoryId=isnull(@itmcatid,I.ItemCategoryId) )*(-1) OUTQTY ,
+//                            (SELECT SUM(S.Quantity)Quantity from StockUpdate S WHERE  StockType='OpeningStock' AND S.ItemId=isnull(@itmid,S.ItemId) AND  S.OrganizationId=@OrganizationId AND I.ItemCategoryId=isnull(@itmcatid,I.ItemCategoryId) ) OPENINGSTOCK
+//                            from StockUpdate S  inner join Item I on I.ItemId=S.ItemId INNER JOIN Unit U ON U.UnitId=I.ItemUnitId
+//                            where S.ItemId=@itmid AND  S.OrganizationId=@OrganizationId AND I.ItemCategoryId=@itmcatid
+//	                          AND S.StockPointId=ISNULL(NULLIF(@stkid,0),S.StockPointId)
+//							  AND I.ItemGroupId=ISNULL(NULLIF(@ItemGroupId,0),I.ItemGroupId)
+//							  AND I.ItemSubGroupId=ISNULL(NULLIF(@ItemSubGroupId, 0), I.ItemSubGroupId)
+//							  AND I.ItemId=ISNULL(NULLIF(@itmid, 0), I.ItemId)
+//                            and I.PartNo=ISNULL(NULLIF(@PartNo, 0), I.PartNo)
+//							  GROUP BY  I.ItemId,I.ItemRefNo,I.ItemName,ItemCategoryId,U.UnitName";
+
                 return connection.Query<ClosingStock>(qry, new { itmcatid = itmcatid, stkid = stkid, itmid = itmid, OrganizationId = OrganizationId, from = from, to = to, ItemGroupId = ItemGroupId, ItemSubGroupId = ItemSubGroupId, PartNo = PartNo }).ToList();
 
                
