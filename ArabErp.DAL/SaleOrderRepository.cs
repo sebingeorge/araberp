6c86a45f6d7ff3cd81ad40feaed9f858ae1d4ce0
Midhu George <MidhuGeorge@gmail.com>
2017-01-17 09:52:27 +0530

    service enquiry previous

--- ArabErp.DAL/SaleOrderRepository.cs
+++ ArabErp.DAL/SaleOrderRepository.cs
@@ -1092,7 +1092,7 @@ namespace ArabErp.DAL
                 string query = @"SELECT ServiceEnquiryId, ServiceEnquiryRefNo, ServiceEnquiryDate, VehicleMake, BoxMake, FreezerMake, TailLiftMake, C.CustomerName,
                                ISNULL([VehicleRegNo],'')RegistrationNo ,ISNULL( [VehicleChassisNo],'') ChassisNo,FreezerModel
                                 FROM ServiceEnquiry SE INNER JOIN Customer C ON SE.CustomerId = C.CustomerId
-                                WHERE SE.OrganizationId= @OrganizationId order by ServiceEnquiryId desc";
+                                WHERE SE.OrganizationId= @OrganizationId and SE.isProjectBased=0  order by ServiceEnquiryId desc";
                 return connection.Query<ServiceEnquiry>(query, new { OrganizationId = OrganizationId }).ToList();
             }
         }
