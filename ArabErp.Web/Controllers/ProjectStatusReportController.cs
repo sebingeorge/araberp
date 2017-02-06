1d33b2f6d72e038981c606a8811e97ceb7bdd486
Praseetha Kuriakose <vivelashani>
2017-02-06 17:35:32 +0530

    ProjectStatusReport

---
+++ ArabErp.Web/Controllers/ProjectStatusReportController.cs
@@ -0,0 +1,31 @@
+﻿using ArabErp.DAL;
+using ArabErp.Domain;
+using ArabErp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using CrystalDecisions.CrystalReports.Engine;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ArabErp.Web.Controllers
+{
+    public class ProjectStatusReportController : BaseController
+    {
+        // GET: ProjectStatusReport
+        public ActionResult Index()
+        {
+            FillJobCard();
+            return View();
+        }
+
+        public void FillJobCard()
+        {
+            ViewBag.JCList = new SelectList(new DropdownRepository().JobCardDropdown(OrganizationId), "Id", "Name");
+        }
+    }
+}
